50c4db8c5bd81b58ad8c2fcfd91ffdb00f12a3a4
unko-chan <kevinliang0303@gmail.com>
2021-01-29 00:13:28 -0800

    new icons

--- server/lib/util/user-helper.js
+++ server/lib/util/user-helper.js
@@ -29,8 +29,8 @@ module.exports = {
    
     const avatars = {
     
-      Female: ["https://i.imgur.com/nlhLi3I.png","https://i.imgur.com/z5LNkkB.png","https://i.imgur.com/v0JXau2.png","https://i.imgur.com/lRUnDgU.png", "https://i.imgur.com/3GvwNBf.png"],
-      Male: ["https://i.imgur.com/73hZDYK.png","https://i.imgur.com/5fUVPRP.png","https://i.imgur.com/DVpDmdR.png","https://i.imgur.com/2WZtOD6.png", "https://i.imgur.com/ilT4JDe.png"]
+      Female: ["https://i.imgur.com/yaktK6q.png","https://i.imgur.com/KZgbR05.png","https://i.imgur.com/AzWPj5K.png","https://i.imgur.com/4oJyL9W.png", "https://i.imgur.com/8JXCIH1.png"],
+      Male: ["https://i.imgur.com/l1Kyki7.png","https://i.imgur.com/GukyDiR.png","https://i.imgur.com/Rfpv0Tw.png","https://i.imgur.com/30X6f8Y.png", "https://i.imgur.com/am3I9Ov.png"]
     
     }
     
